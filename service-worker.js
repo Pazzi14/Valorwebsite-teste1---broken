653155d790adf31905310d4b0bfea58b9e37fd18
PAZZI14 <pazinatto.tiago@gmail.com>
2024-08-02 12:34:18 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'valor-financiamentos-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/images/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
