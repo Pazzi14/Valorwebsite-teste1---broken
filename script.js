66a5ee873c7e62c93f60320d026828698660f39a
PAZZI14 <pazinatto.tiago@gmail.com>
2024-08-01 21:55:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -126,24 +126,4 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
-
-    // Animação de Scroll
-    function reveal() {
-        var reveals = document.querySelectorAll(".reveal");
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 150;
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-
-    window.addEventListener("scroll", reveal);
-
-    // Para ativar a animação na carga inicial
-    reveal();
 });
